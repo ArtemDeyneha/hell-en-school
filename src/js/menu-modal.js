37d0672b0f23275ea9267617cd961f8c40d739dd
artem <73790146+ArtemDeynega@users.noreply.github.com>
2022-08-31 21:46:43 +0300

    1

---
+++ src/js/menu-modal.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  $('.menu-button').click(function (event) {
+    $('.menu-button-icon,.burger-menu').toggleClass('menu-open');
+    $('body').toggleClass('lock');
+    $('.logo').toggleClass('inverse');
+  });
+
+  $('.header-mobile-btn').click(function (event) {
+    $('.backdrop').removeClass('is-hidden');
+    $('.modal-window').removeClass('animate__bounceOutRight');
+    $('.modal-window').toggleClass('animate__bounceInRight');
+  });
+
+  $('.header-btn').click(function (event) {
+    $('.backdrop').removeClass('is-hidden');
+    $('.modal-window').removeClass('animate__bounceOutRight');
+    $('.modal-window').toggleClass('animate__bounceInRight');
+    $('body').toggleClass('lock');
+  });
+
+  $('.close-btn-mobile').click(function (event) {
+    $('.backdrop').toggleClass('is-hidden');
+    $('.modal-window').removeClass('animate__bounceInRight');
+    $('.modal-window').toggleClass('animate__bounceOutRight');
+  });
+
+  $('.close-btn').click(function (event) {
+    $('.backdrop').toggleClass('is-hidden');
+    $('.modal-window').removeClass('animate__bounceInRight');
+    $('.modal-window').toggleClass('animate__bounceOutRight');
+    $('body').removeClass('lock');
+  });
+
+  $('.nav-link').click(function (event) {
+    $('.burger-menu').removeClass('menu-open');
+    $('.logo').removeClass('inverse');
+    $('.menu-button-icon,.burger-menu').removeClass('menu-open');
+    $('body').removeClass('lock');
+  });
+});
---
+++ src/js/return-btn.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let gototop = document.querySelector('.uptop');
+  let body = document.documentElement;
+
+  window.addEventListener('scroll', check);
+
+  function check() {
+    pageYOffset >= 500 && gototop.classList.add('upview');
+    pageYOffset < 500 && gototop.classList.remove('upview');
+  }
+
+  gototop.onclick = function () {
+    animate({
+      duration: 700,
+      timing: gogototopEaseOut,
+      draw: progress => (body.scrollTop = body.scrollTop * (1 - progress / 7)),
+    });
+  };
+
+  let circ = timeFraction =>
+    1 -
+    Math.sin(Math.acos(timeFraction > 1 ? (timeFraction = 1) : timeFraction));
+
+  let makeEaseOut = timing => timeFraction => 1 - timing(1 - timeFraction);
+  let gogototopEaseOut = makeEaseOut(circ);
+});
+
+function animate(options) {
+  let start = performance.now();
+
+  requestAnimationFrame(function animate(time) {
+    let timeFraction = (time - start) / options.duration;
+    timeFraction > 1 && (timeFraction = 1);
+
+    let progress = options.timing(timeFraction);
+
+    options.draw(progress);
+    timeFraction < 1 && requestAnimationFrame(animate);
+  });
+}
